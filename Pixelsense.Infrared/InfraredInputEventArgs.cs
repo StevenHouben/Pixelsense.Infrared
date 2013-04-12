a424a9a731fce119c4706be1cf5e15273e78d93b
Steven Houben <shou@itu.dk>
2013-04-12 16:27:36 +0200

    Initial commit

---
+++ Pixelsense.Infrared/InfraredInputEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pixelsense.Infrared
+{
+    public class InfraredInputEventArgs:EventArgs
+    {
+        public InfraredInput Led { get; set; }
+        public InfraredInputEventArgs(InfraredInput led)
+        {
+            Led = led;
+        }
+    }
+}
